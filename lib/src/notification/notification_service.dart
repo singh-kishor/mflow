28edf33cf34bc9feb3a99690b4da4442498fea40
Kishor Singh <kishor.nitb@gmail.com>
2021-09-19 11:25:07 +0530

    temp changes

---
+++ lib/src/notification/notification_service.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationService{
+  static final NotificationService _notificationService = NotificationService._internal();
+
+  factory NotificationService() => _notificationService;
+
+  NotificationService._internal();
+}
